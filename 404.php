69e325ec8e553c26729a9f0e238096c4a408b06a
profih <99394988+profih@users.noreply.github.com>
2022-11-08 11:01:03 +0300

    Добавил теги
    Добавил для стиля теги: <span>, <i>

--- 404.php
+++ 404.php
@@ -10,14 +10,14 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Страница не найдена");?>
 
     <div class="container-404">
-        <div class="container-404__code">404</div>
+        <div class="container-404__code"><span>404</span></div>
         <div class="container-404__text">
             <span>К сожалению, такой страницы не существует.<br> Возможно, вы неправильно набрали адрес.
             </span>
         </div>
         <img src='/upload/404.png' class="container-404__img">
-        <div class="container-404__quote">
+        <div class="container-404__quote"><span><i>
             «Чтобы капал самогон мне в рот<br> днем и ночью круглый год!»
-        </div>
+        </span></i></div>
     </div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
